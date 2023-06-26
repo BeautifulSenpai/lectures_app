b70b5398a3f1359a349e6674a66e0c9d1f681c01
Senpai <maraev.515c@mail.ru>
2023-06-26 14:17:05 +0300

    123456

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lectures App',
+      title: 'Lectures App123',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
